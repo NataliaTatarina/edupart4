dc4724b9447a6c77f2a3f33810b6293945b613a7
Natatatarina <nata-tatarina@mail.ru>
2022-07-20 16:48:38 +0300

    Commit6

--- src/test/java/Mesto1Test.java
+++ src/test/java/Mesto1Test.java
@@ -33,7 +33,7 @@ public class Mesto1Test {
     @DisplayName("Like the first photo")
     @Description("This test is for liking the first photo on Mesto.")
     public void likeTheFirstPhotoTest() {
-        String photoId = getTheFirstPhotoId();
+        String photoId = getTheFirstPhotoId()+6;
 
         likePhotoById(photoId);
         deleteLikePhotoById(photoId);
